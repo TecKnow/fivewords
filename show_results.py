aeba8a44ea9aacd5b30cfbc1b9f903f85cc11677
David P. Perkins <tecknow@grumbleware.com>
2022-09-07 20:35:40 -0500

    Generate the qualifying sets of words.

--- show_results.py
+++ show_results.py
@@ -1,46 +1,32 @@
-import typing
-import graphviz
 from collections import deque
+from itertools import chain
+from pprint import pprint
 
 
-def stringify_set(letter_set: set[str]) -> str:
-    return "".join(sorted(letter_set))
+def get_longest_element(s):
+    return max(s, key=len)
 
 
-T = typing.TypeVar("T", graphviz.Graph, graphviz.Digraph)
-
-
-def graph_slice(starting_points: typing.Iterable[str],
-                data: typing.Mapping[frozenset[str], frozenset[str]] | frozenset[frozenset[frozenset[str]]],
-                graph: T) -> T:
-    work: deque[str | frozenset[str] | frozenset[frozenset[frozenset[str]]]] = deque(starting_points)
+def expand_answers(starting_points, data):
+    work = deque(chain.from_iterable(starting_points))
     while work:
-        k = work.pop()
-        v = data[k]
-        string_key = stringify_set(k)
-        if len(k) == 5:
-            pass
-            for word in v:
-                graph.edge(string_key, word)
+        item = work.pop()
+        longest_element = get_longest_element(item)
+        if len(longest_element) > 5:
+            new_item_base = item.difference(frozenset({longest_element}))
+            new_components = data[longest_element]
+            for new_component in new_components:
+                new_item = new_item_base.union(new_component)
+                work.appendleft(new_item)
         else:
-            for a, b in v:
-                str_a = stringify_set(a)
-                str_b = stringify_set(b)
-                str_b, str_a = sorted((str_a, str_b), key= lambda x: (len(x), x))
-                if str_a == str_b:
-                    print(f"a, b self-reference found: {str_a=}, {str_b=}, {k=}")
-                else:
-                    graph.edge(str_a, str_b)
-                if str_a == string_key:
-                    print(f"a, k self-reference found: {str_a=}, {str_b=}, {k=}")
-                else:
-                    graph.edge(string_key, str_a)
-                work.extendleft((a, b))
-    return graph
+            yield frozenset((data[x] for x in item))
 
 
 if __name__ == "__main__":
     results = eval(open("data/results.py").read())
-    neat = graphviz.Digraph("five_words", strict=True, format="png")
-    neat = graph_slice([x for x in results.keys() if len(x) == 25][0:1], results, neat)
-    neat.render(directory="data", view=True)
+    answers = [v for k, v in results.items() if len(k) == 25]
+    l = frozenset(expand_answers(answers, results))
+    from pprint import pprint
+    pprint(l)
+    print()
+    print(len(l))
